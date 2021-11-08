543617594c20050ed4531a204d94ece447c4ea13
beshahadat <shahadatnayeem23@gmail.com>
2021-11-09 03:39:02 +0600

    Lorem in the main function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('testing.png')
+img = cv2.imread('Lorem.png')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 #Detecting Characters
