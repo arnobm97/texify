586198064bb74353efdb31f82399b143a5568d09
beshahadat <shahadatnayeem23@gmail.com>
2021-11-10 06:59:12 +0600

    searching specific word features added

--- search.py
+++ search.py
@@ -15,29 +15,28 @@ boxes = pytesseract.image_to_boxes(img)
 search = []
 result = []
 count = 0
+sCount = 0
 
 for b in boxes.splitlines():
-    # print(b)
-    b = b.split(' ')
-    #print(b)
-    x,y,w,h,= int(b[1]),int(b[2]),int(b[3]),int(b[4],)
     s = b[0]
     search.append(s)
-
-print(search)
+    sCount += 1
 
 for i in range(len(search)):
     if search[i] == Q:
         result.append(search[i])
         count += 1
-    # else:
-    #     print("Your desired letter is not found")
 
 print(result)
 print("Your desired letter which is", Q , "is found", count , "times")
 
-#     cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),1)
-#     cv2.putText(img,b[0],(x,hImg-y+20),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,255),1)
-#
-# cv2.imshow('Result',img)
-# cv2.waitKey(0)
+
+for b in boxes.splitlines():
+    if (b[0]== Q):
+        b = b.split(' ')
+        x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+        cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),1)
+        cv2.putText(img,b[0],(x,hImg-y+20),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,255),1)
+
+cv2.imshow('Result',img)
+cv2.waitKey(0)
